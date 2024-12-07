022d66d6d38172351fe87e535c94383e81089c11
shutterscaped <shutterscaped@gmail.com>
2024-12-07 01:19:32 +0000

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -17,3 +17,25 @@ export const nav = [
 		slug: '/company/contact',
 	},
 ];
+
+
+<p class="nav-bar" >Home< />
+
+<style>
+
+	       .nav-bar {
+		width: fit-content;
+                height: ;
+		margin: ;
+                position: absolute;
+                top: px
+                left: 0;
+		font-family: var(--theme-font-family-serif);
+    font-size: var(--font-size-md);
+		color: inherit;
+	}
+	.odyssey-logo:hover {
+		text-decoration: underline;
+		cursor: pointer;
+	}
+</style>
